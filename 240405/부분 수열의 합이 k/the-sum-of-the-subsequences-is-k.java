11a2d1825e60677aed67743c938eb5140cd5588e
heypli <baejisuk524@gmail.com>
2024-04-05 21:47:45 +0900

    Title: 부분 수열의 합이 k, Time: 203ms, Memory: 13MB, Status: Passed - CodeTree

--- 240405/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.java
+++ 240405/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.java
@@ -15,19 +15,21 @@ public class Main {
 
         arr = new int[n+1];
         prefixSum = new int[n+1];
-        
+
         for(int i = 1; i < n+1; i++) {
             arr[i] = sc.nextInt();
             prefixSum[i] = prefixSum[i-1] + arr[i];
         }
 
         ans = 0;
-        for(int i = 1; i < n ; i++) {
+        for(int i = 1; i <= n ; i++) {
+            if(arr[i] == k) ans++;
             for(int j = i+1; j <= n; j++) {
                 int result = prefixSum[j] - prefixSum[i] + arr[i];
                 if(result == k) ans++;
             }
         }
+
 
         System.out.println(ans);
     }
