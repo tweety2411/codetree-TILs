87fa9f12dfb6fb3407b6a945640069ae15ece5cf
heypli <baejisuk524@gmail.com>
2024-04-05 15:44:05 +0900

    Title: 강강수월래, Time: 3095ms, Memory: 37MB, Status: Time Limit Exceed - CodeTree

--- 240405/강강수월래/circle-dance.java
+++ 240405/강강수월래/circle-dance.java
@@ -1,12 +1,10 @@
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 public class Main {
     public static int n, m;
     static Node[] nodes;
-    static Circle[] circles;
-    static HashMap<Integer, Integer> lineNums;
+    static HashMap<Integer, Integer> studentIds;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,19 +12,18 @@ public class Main {
         m = sc.nextInt();
         int q = sc.nextInt();
         nodes = new Node[100_002];
-        circles = new Circle[11];
-        lineNums = new HashMap<>();
+        studentIds = new HashMap<>();
 
+        int nodeCount = 1;
         for (int i = 1; i < m + 1; i++) {
             int num = sc.nextInt();
-            TreeSet<Integer> sets = new TreeSet<>();
             Node prev = null;
             Node head = null;
             for (int j = 1; j < num + 1; j++) {
                 int data = sc.nextInt();
                 Node node = new Node(data);
-                nodes[data] = node;
-                lineNums.put(data, i);
+                nodes[nodeCount] = node;
+                studentIds.put(data, nodeCount);
                 if (j == 1) {
                     head = node;
                 } else {
@@ -35,10 +32,8 @@ public class Main {
                         connect(node, head);
                 }
                 prev = node;
-                sets.add(data);
+                nodeCount++;
             }
-            Circle circle = new Circle(i, sets);
-            circles[i] = circle;
         }
 
         while (q-- > 0) {
@@ -48,15 +43,7 @@ public class Main {
                 int b = sc.nextInt();
 
                 connectCircle(a, b);
-                int a_line = lineNums.get(a);
-                int b_line = lineNums.get(b);
-                Circle circle = circles[b_line];
-                for (Integer x : circle.sets) {
-                    lineNums.put(x, a_line);
-                }
 
-                circles[a_line].sets.addAll(circle.sets);
-                circles[b_line] = null;
             } else if (opt == 2) { // 분리하기
                 int a = sc.nextInt();
                 int b = sc.nextInt();
@@ -65,54 +52,46 @@ public class Main {
             } else if (opt == 3) {
                 int a = sc.nextInt();
 
-                for (int i = 1; i < m + 1; i++) {
-                    if (circles[i].sets.contains(a)) {
-                        int first = circles[i].sets.first();
-                        Node cur = nodes[first];
-                        System.out.print(cur.data + " ");
-                        cur = cur.prev;
-                        while (cur != nodes[first]) {
-                            System.out.print(cur.data + " ");
-                            cur = cur.prev;
-                        }
-                    }
+                Node cur = nodes[studentIds.get(a)];
+                int min = Integer.MAX_VALUE;
+                Node target = cur;
+                while (true) {
+                    cur = cur.next;
+                    if (cur != null)
+                        min = Math.min(min, cur.data);
+                    if (cur == target)
+                        break;
                 }
+
+                Node minNode = nodes[studentIds.get(min)];
+                cur = minNode;
+                do {
+                    System.out.print(cur.data + " ");
+                    cur = cur.prev;
+                } while (cur.data != minNode.data);
+
+                System.out.println();
             }
         }
-
     }
 
     private static void seperateCircle(int a, int b) {
-        Node cur = nodes[a];
-        TreeSet<Integer> sets = new TreeSet<>();
-        Circle orgCircle = circles[lineNums.get(b)];
-        TreeSet<Integer> orgSets = orgCircle.sets;
-        int lineNum = 1;
-        while (circles[lineNum] != null && lineNum < m + 1) {
-            lineNum++;
-        }
+        Node cur = nodes[studentIds.get(a)];
         while (cur.data != b) {
-            sets.add(cur.data);
-            lineNums.put(cur.data, lineNum);
-            orgSets.remove(cur.data);
             cur = cur.next;
 
         }
         Node last = cur.prev;
         int x = last.data;
 
-        orgCircle.sets = orgSets;
-        circles[lineNums.get(b)] = orgCircle;
-        circles[lineNum] = new Circle(lineNum, sets);
-
-        connect(nodes[a].prev, nodes[x].next);
-        connect(nodes[x], nodes[a]);
+        connect(nodes[studentIds.get(a)].prev, nodes[studentIds.get(x)].next);
+        connect(nodes[studentIds.get(x)], nodes[studentIds.get(a)]);
 
     }
 
     private static void connectCircle(int a, int b) {
-        connect(nodes[b].prev, nodes[a].next);
-        connect(nodes[a], nodes[b]);
+        connect(nodes[studentIds.get(b)].prev, nodes[studentIds.get(a)].next);
+        connect(nodes[studentIds.get(a)], nodes[studentIds.get(b)]);
 
     }
 
@@ -133,15 +112,5 @@ class Node {
     public Node(int data) {
         this.data = data;
         this.prev = this.next = null;
-    }
-}
-
-class Circle {
-    int idx;
-    TreeSet<Integer> sets;
-
-    public Circle(int idx, TreeSet<Integer> sets) {
-        this.idx = idx;
-        this.sets = sets;
     }
 }
