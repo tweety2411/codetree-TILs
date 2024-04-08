32fb8a772eee1c4e0211198c21c4169a51fb8176
heypli <baejisuk524@gmail.com>
2024-04-08 13:25:44 +0900

    Title: 최대 직사각형 합, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240408/최대 직사각형 합/max-rect-sum-in-grid.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int n, ans;
+    static int[][] arr;
+    static int[][] prefixSum;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        arr = new int[n + 1][n + 1];
+        prefixSum = new int[n + 1][n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                arr[i][j] = sc.nextInt();
+                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = i; j < n + 1; j++) {
+                ans = getMaxArea(i, j);
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static int getMaxArea(int x1, int x2) {
+
+        int max = Integer.MIN_VALUE;
+        int[] dp = new int[n + 1];
+        for (int y = 1; y < n + 1; y++) {
+            int result = getSum(x1, y, x2, y);
+
+            dp[y] = Math.max(result, dp[y - 1] + result);
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    private static int getSum(int x1, int y1, int x2, int y2) {
+        return prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+    }
+
+}
