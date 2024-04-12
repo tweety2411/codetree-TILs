bab4c0fde1c12df4641cae724dfdf45d2060c193
heypli <baejisuk524@gmail.com>
2024-04-12 14:43:59 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 814ms, Memory: 33MB, Status: Passed - CodeTree

--- 240412/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.java
+++ 240412/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.java
@@ -28,20 +28,21 @@ public class Main {
 
         // L 배열을 채워줍니다.
         // L[i] = 0번부터 i번 원소 중 최댓값
-        for (int i = 1; i < n + 1; i++) {
+        L[0] = arr[0];
+        for (int i = 1; i < n; i++) {
             L[i] = Math.max(L[i - 1], arr[i]);
         }
 
-           // R 배열을 채워줍니다.
+        // R 배열을 채워줍니다.
         // R[i] = i번부터 n - 1번 원소 중 최댓값
-        for (int i = n - 1; i > 0; i--) {
+        R[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
             R[i] = Math.max(R[i + 1], arr[i]);
         }
 
-        
         // i번째 숫자가 세 숫자 중 가운데 숫자라고 했을 때
         // 가능한 최대 합 중 최댓값을 갱신해줍니다.
-        for (int i = 2; i < n - 1; i++) {
+        for (int i = 2; i < n - 2; i++) {
             ans = Math.max(ans, L[i - 2] + arr[i] + R[i + 2]);
         }
 
