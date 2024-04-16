313d9497953eaa2a85aeae08653d524fb69e8cc6
heypli <baejisuk524@gmail.com>
2024-04-16 21:55:19 +0900

    Title: 최대 숫자 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240416/최대 숫자 만들기/make-biggest-num.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
+public class Main {
+
+	static int n, m, ans;
+	static int[] arr;
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		
+		arr = new int[n];
+		
+		String[] arrs = new String[n];
+		
+		for(int i = 0; i < n ; i++) {
+			arrs[i] = String.valueOf(sc.nextInt());
+		}
+		
+		 Arrays.sort(arrs, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				return Integer.valueOf(o2+o1) - Integer.valueOf(o1+o2);
+			}
+		});
+		
+		 StringBuilder sb = new StringBuilder();
+		 for(int i = 0; i < n; i++) {
+			 sb.append(String.valueOf(arrs[i]));
+		 }
+		 
+		 System.out.println(sb.toString());
+		 
+	}
+
+}
