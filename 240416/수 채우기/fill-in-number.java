e8dc9a25c7cbbdff401abb56c12244abb1e1c19c
heypli <baejisuk524@gmail.com>
2024-04-16 22:47:53 +0900

    Title: 수 채우기, Time: 3089ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240416/수 채우기/fill-in-number.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	static int n, m, ans;
+	static int[] arr;
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+
+		arr = new int[n+1];
+		
+		int last = n;
+		ans = 0;
+		while( last > 0) {
+			if(last >= 5) {
+				if(last % 5 == 0) {
+					ans += last / 5;
+					break;
+				}
+				last = last - 2;
+				ans++;
+			} else {
+				
+				if(last % 2 == 0) {
+					ans += last / 2;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(ans);
+		 
+	}
+
+}
