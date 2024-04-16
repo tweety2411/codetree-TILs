e334fe026120356d01f0d6fa27546bfccde34f02
heypli <baejisuk524@gmail.com>
2024-04-16 23:06:17 +0900

    Title: 수 채우기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240416/수 채우기/fill-in-number.java
+++ 240416/수 채우기/fill-in-number.java
@@ -1,41 +1,30 @@
 import java.util.Scanner;
 
-
 public class Main {
+    public static final int MAX_NUM = 100000;
+    
+    // 변수 선언
+    public static int n;
+    public static int ans = MAX_NUM;
 
-	static int n, m, ans;
-	static int[] arr;
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
 
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
+        // 사용할 5원 동전의 수를 전부 가정해보며
+        // 그 중 가장 좋은 선택을 합니다.
+        for(int i = 0; i <= MAX_NUM; i++) {
+            int remainder = n - 5 * i;
+            if(remainder >= 0 && remainder % 2 == 0)
+                ans = Math.min(ans, i + (remainder / 2));
+        }
 
-			
-		int last = n;
-		ans = 0;
-		if(last < 0) ans = -1;
-		while( last > -1) {
-			if(last >= 5) {
-				if(last % 5 == 0) {
-					ans += last / 5;
-					break;
-				}
-				last = last - 2;
-				ans++;
-			} else {
-				
-				if(last % 2 == 0) {
-					ans += last / 2;
-					break;
-				}
-			}
-		}
-		
-		
-		
-		System.out.println(ans);
-		 
-	}
+        // 만약 그러한 동전을 만드는 것이 불가능하다면
+        // -1을 넣어줍니다.
+        if(ans == MAX_NUM)
+            ans = -1;
 
+        System.out.print(ans);
+    }
 }
