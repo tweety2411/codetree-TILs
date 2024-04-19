f67f17c46f88aadb11f70ebc7adabd8b7782063d
heypli <baejisuk524@gmail.com>
2024-04-19 09:42:19 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240419/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+
+    static int n, m, ans;
+    static int[] arr;
+
+    static TreeSet<Integer> sets;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        sets = new TreeSet<>();
+        for (int i = 0; i < n * 2; i++) {
+            sets.add(i + 1);
+        }
+
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sets.remove(arr[i]);
+        }
+
+        ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (null != sets.higher(arr[i])) {
+                int a = sets.higher(arr[i]);
+                ans += a - arr[i];
+                sets.remove(sets.higher(arr[i]));
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
